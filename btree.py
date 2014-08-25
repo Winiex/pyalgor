730af73ae977b41d85c0095e0462916b61e847a6
winiex <winiex.nie@gmail.com>
2014-08-26 00:58:22 +0800

    Init.

---
+++ btree.py
@@ -0,0 +1,34 @@
+class BTNode(object):
+
+    def __init__(self):
+        self.left = None
+        self.left = None
+        self.value = None
+
+    @property
+    def left(self):
+        return self.left
+
+    @left.setter
+    def left(self, left):
+        self.left = left
+
+    @property
+    def right(self):
+        return self.right
+
+    @right.setter
+    def right(self, right):
+        self.right = right
+
+    @property
+    def value(self):
+        return self.value
+
+    @value.setter
+    def value(self, left):
+        self.left = left
+
+
+class BTree(object):
+    pass
