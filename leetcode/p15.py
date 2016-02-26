752e875c0e87213efa6b14e7bb4116cd0a2c551b
winiex <winiex@imaygou.com>
2016-02-26 18:45:40 +0800

    Backup

---
+++ leetcode/p15.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# https://leetcode.com/problems/3sum/
+
+
+class Solution(object):
+
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
